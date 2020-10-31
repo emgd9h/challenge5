5846d80aac988c6a2732093bc82829cbce77cc58
Evan G <lilgersh01@gmail.com>
2020-10-30 22:15:11 -0500

    Updated log page

--- src/app/pages/logFile/checkout-log/checkout-log.page.ts
+++ src/app/pages/logFile/checkout-log/checkout-log.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from "src/app/services/ChecklistData/data.service";
+import { ListItem } from 'src/app/types/checkoutList/list-item';
 
 @Component({
   selector: 'app-checkout-log',
@@ -7,17 +8,21 @@ import { DataService } from "src/app/services/ChecklistData/data.service";
   styleUrls: ['./checkout-log.page.scss'],
 })
 export class CheckoutLogPage implements OnInit {
-  headphoneLogArray = [];
-  private updateTheLog(){
-    this.headphoneLogArray = this.ChecklistModel.updateLog();
-  }
+  headphoneLogArray: ListItem[] = [];
+
 
   constructor(private ChecklistModel: DataService) {
+    this.ChecklistModel.getData("checkoutLog").then((checkouts) => {
+      if (checkouts) {
+        this.headphoneLogArray = checkouts;
+      }
+
+    });
     
    }
 
   ngOnInit() {
-    this.updateTheLog();
+  
   }
   
 
